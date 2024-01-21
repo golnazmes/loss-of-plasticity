baf29bfcc29f7bf4e07da85e6c61c7494c5aa028
Golnazmes <gmesbahi.gm@gmail.com>
2024-01-21 13:20:09 -0700

    changed bash files for CC

--- lop/slowly_changing_regression/prerun.sh
+++ lop/slowly_changing_regression/prerun.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-cd ~/projects/def-amw8/mesbahi/los-of-plasticity/lop/slowly_changing_regression
+cd ~/projects/def-amw8/mesbahi/loss-of-plasticity/lop/slowly_changing_regression
 module load python/3.9
-source /home/mesbahi/projects/def-amw8/mesbahi/los-of-plasticity/venv/bin/activate
+source /home/mesbahi/projects/def-amw8/mesbahi/loss-of-plasticity/venv/bin/activate
+
 
 
 #create 100 json files in env_temp_cfg
 mkdir env_temp_cfg temp_cfg
-python multi_param_expr.py -c cfg/prob.json 
+/home/mesbahi/projects/def-amw8/mesbahi/loss-of-plasticity/venv/bin/python3.9 multi_param_expr.py -c cfg/prob.json 
 
 #generate dataset for the json files
 for i in {0..99}
 do
-    python slowly_changing_regression.py -c env_temp_cfg/$i.json 
+    /home/mesbahi/projects/def-amw8/mesbahi/loss-of-plasticity/venv/bin/python3.9 slowly_changing_regression.py -c env_temp_cfg/$i.json 
 done
 
 #create 100 json files for learning models
-python multi_param_expr.py -c cfg/sgd/bp/relu.json 
+/home/mesbahi/projects/def-amw8/mesbahi/loss-of-plasticity/venv/bin/python3.9 multi_param_expr.py -c cfg/sgd/bp/relu.json 
--- lop/slowly_changing_regression/run.sh
+++ lop/slowly_changing_regression/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --array=0-99
+#SBATCH --array=3-99
 #SBATCH --output=Array_test.%A_%a.out
 #SBATCH --error=Array_test.%A_%a.error
 #SBATCH --job-name=sgd
 
 
 
-#SBATCH --mem=15G
+#SBATCH --mem=3G
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-2:59:59
+#SBATCH --time=0-0:59:59
 
 
 #SBATCH --mail-user=<mesbahi@ualberta.ca>
@@ -21,4 +21,4 @@ module load python/3.9
 source /home/mesbahi/projects/def-amw8/mesbahi/los-of-plasticity/venv/bin/activate
 
 #run the models on them
-python expr.py -c temp_cfg/$SLURM_ARRAY_TASK_ID.json 
+/home/mesbahi/projects/def-amw8/mesbahi/loss-of-plasticity/venv/bin/python3.9 expr.py -c temp_cfg/$SLURM_ARRAY_TASK_ID.json 
